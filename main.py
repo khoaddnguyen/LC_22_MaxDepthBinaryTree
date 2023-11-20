d107b358defca4a0c69a7417843ce5ed96af9650
kwa_laa <khoaddnguyen@gmail.com>
2023-11-20 13:45:16 -0600

    iterative DFS method

--- main.py
+++ main.py
@@ -11,21 +11,32 @@ def maxDepth(self, root: Optional[TreeNode]) -> int:
 
     # BFS - traverse through all levels
 
-    level = 0
-    q = deque([root])
-    while q:
-
-        for i in range(len(q)):
-            node = q.popleft()
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-        level += 1
-    return level
-
+    # level = 0
+    # q = deque([root])
+    # while q:
+    #
+    #     for i in range(len(q)):
+    #         node = q.popleft()
+    #         if node.left:
+    #             q.append(node.left)
+    #         if node.right:
+    #             q.append(node.right)
+    #     level += 1
+    # return level
 
 
 
     # Iterative DFS - DFS without recursion
 
+    stack = [[root, 1]]
+    result = 1
+
+    while stack:
+        node, depth = stack.pop()
+
+        if node:
+            result = max(result, depth)
+            stack.append([node.left, depth + 1])
+            stack.append([node.right, depth + 1])
+
+    return result
