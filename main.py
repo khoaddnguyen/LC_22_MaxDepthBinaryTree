aec8308620f1f67e4698de0fd286bb1d995d1ae9
kwa_laa <khoaddnguyen@gmail.com>
2023-11-20 13:28:23 -0600

    BFS method

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 def maxDepth(self, root: Optional[TreeNode]) -> int:
-    # recursive
-    if not root:
-        return 0
 
-    return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    if not root:
+         return 0
+
+    # DFS recursion: [1 + max(dfs(left), dfs(right))]
+
+    # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+
+    # BFS - traverse through all levels
+
+    level = 0
+    q = deque([root])
+    while q:
+
+        for i in range(len(q)):
+            node = q.popleft()
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        level += 1
+    return level
+
+
+
+
+    # Iterative DFS - DFS without recursion
+
